5389cbc88e86628931e34512a2f3146a97a3f6ea
shamal <shamalsundar2000@gmail.com>
2023-07-10 22:47:10 +0530

    new

---
+++ src/fun.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+const Fun = () => {
+  return (
+    <div>
+      <h1>KERALA</h1>
+
+      <div className="main">
+        <img
+          className="image"
+          src="https://slbckerala.com/images/Kerala_map.jpg"
+        ></img>
+        <div className="main1">
+          <p>
+            A substantial portion of Kerala including the western coastal
+            lowlands and the plains of the midland may have been under the sea
+            in ancient times. Marine fossils have been found in an area near
+            Changanassery, thus supporting the hypothesis.[77] Pre-historical
+            archaeological findings include dolmens of the Neolithic era in the
+            Marayur area of the Idukki district, which lie on the eastern
+            highland made by Western Ghats. They are locally known as
+            "muniyara", derived from muni (hermit or sage) and ara (dolmen).[78]
+            Rock engravings in the Edakkal Caves, in Wayanad date back to the
+            Neolithic era around 6000 BCE.[79][80] Archaeological studies have
+            identified Mesolithic, Neolithic and Megalithic sites in Kerala.[81]
+            The studies point to the development of ancient Kerala society and
+            its culture beginning from the Paleolithic Age, through the
+            Mesolithic, Neolithic and Megalithic Ages.[82] Foreign cultural
+            contacts have assisted this cultural formation;[83] historians
+            suggest a possible relationship with Indus Valley civilization
+            during the late Bronze Age and early Iron Age.[84]The Eastern world,
+            also known as the East or historically the Orient, is an umbrella
+            term for various cultures or social structures, nations and
+            philosophical systems, which vary depending on the context. It most
+            often includes at least part of Asia or, geographically, the
+            countries and cultures east of Europe, the Mediterranean region and
+            the Arab world, specifically in historical (pre-modern) contexts,
+            and in modern times in the context of Orientalism.[1] It is often
+            seen as a counterpart to the Western world, and correlates strongly
+            to the southern half of the North–South divide.The Eastern world,
+            also known as the East or historically the Orient, is an umbrella
+            term for various cultures or social structures, nations and
+            philosophical systems, which vary depending on the context. It most
+            often includes at least part of Asia or, geographically, the
+            countries and cultures east of Europe, the Mediterranean region and
+            the Arab world, specifically in historical (pre-modern) contexts,
+            and in modern times in the context of Orientalism.[1] It is often
+            seen as a counterpart to the Western world, and correlates strongly
+            to the southern half of the North–South divide.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Fun;
